da3bbedb77f98d872773fe34e2eb703e1cdeaef5
Leoalves007 <leonardo.fuzinelli@aluno.senai.br>
2025-08-08 16:38:11 -0300

    Adiciona suporte ao gerenciamento de tarefas
    - Atualiza `_Layout.cshtml`:
      - Altera idioma para `pt-br` no atributo `lang`.
      - Adiciona link no menu para "Lista de Tarefas".
      - Ajusta formatação e reordena código.
    
    - Adiciona `TarefasController` com método `Index`.
    
    - Adiciona modelo `Tarefa` com propriedades:
      - `TarefaId`, `Titulo`, `Descrição` e `Concluida`.
    
    - Cria view `Index.cshtml` para exibir lista de tarefas.

---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        // Nome da chave primaria deve ser Nome da classe + "Id"
+        public int TarefaId { get; set; } //Id é a chave primária 
+        public string? Titulo { get; set; }
+        public string? Descrição { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
